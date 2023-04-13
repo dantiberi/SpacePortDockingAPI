104d18c2849a600abe6ddfb672e1db039e036199
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-13 13:23:31 -0500

    Created class for reading Json data.

---
+++ Objects/JSONHandler.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SpacePortDockingAPI.Models;
+
+namespace SpacePortDockingAPI.Objects
+{
+    public class JSONHandler
+    {
+        public static AppOptions? appOptions;
+        static string codePath = Environment.CurrentDirectory;
+
+        public static AppOptions LoadOptionsJson()
+        {
+            string path = codePath + "/Res/options.json";
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                AppOptions appOptions = JsonConvert.DeserializeObject<AppOptions>(json);
+
+                if(appOptions == null) throw new IOException($"options.json not found at {path}");
+
+                return appOptions;
+            }
+        }
+    }
+}

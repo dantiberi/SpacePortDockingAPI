2706478b5081ff7b36171a00293e4b477ccdb624
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-13 13:23:45 -0500

    Model for application options.

---
+++ Models/Options.cs
@@ -0,0 +1,13 @@
+﻿namespace SpacePortDockingAPI.Models
+{
+    //https://json2csharp.com/
+    public class Database
+    {
+        public string dbFileName { get; set; }
+    }
+
+    public class AppOptions
+    {
+        public Database database { get; set; }
+    }
+}

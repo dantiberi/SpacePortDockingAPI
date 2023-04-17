81c93967324ba7b169526350e7866dc394a435e7
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-17 14:59:14 -0500

    Added text content section.

--- Models/Options.cs
+++ Models/Options.cs
@@ -6,8 +6,19 @@
         public string dbFileName { get; set; }
     }
 
+    public class TextContent
+    {
+        public TowerTextContent towerTextContent { get; set; }
+    }
+
+    public class TowerTextContent
+    {
+        public string pingMesssage { get; set; }
+    }
+
     public class AppOptions
     {
         public Database database { get; set; }
+        public TextContent textContent { get; set; }
     }
 }

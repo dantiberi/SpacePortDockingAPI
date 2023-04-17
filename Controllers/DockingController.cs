1b5c6628073c0b173e7ceb3b34b1ea4b5185329c
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-17 15:01:20 -0500

    Create DockingController.cs

---
+++ Controllers/DockingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpacePortDockingAPI.Objects;
+
+namespace SpacePortDockingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DockingController : ControllerBase
+    {
+        [HttpGet]
+        [ActionName("pingTower")]
+        [Route("/tower/ping")]
+        public ActionResult<string> TestJSON()
+        {
+            return JSONHandler.appOptions.textContent.towerTextContent.pingMesssage;
+        }
+    }
+}

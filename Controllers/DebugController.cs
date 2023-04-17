2b14449d3273587e08d3f809b4bcf0647562b860
Dan Tiberi <danny.tiberi@gmail.com>
2023-04-17 14:34:23 -0500

    Created controller for debugging.

---
+++ Controllers/DebugController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpacePortDockingAPI.Objects;
+
+namespace SpacePortDockingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DebugController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> TestJSON()
+        {
+            return Content(JSONHandler.LoadOptionsJson().database.dbFileName, "text/plain");
+        }
+    }
+}
